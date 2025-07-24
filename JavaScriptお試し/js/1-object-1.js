fb64291a6425fe8d7e65d6129de6cdcabe7e0b98
ルキア <chii-0301r@docomo.ne.jp>
2025-07-24 18:46:40 +0900

    Add files via upload

---
+++ JavaScriptお試し/js/1-object-1.js
@@ -0,0 +1,9 @@
+const user = {
+    userName: "ルキア",
+    userAge: 20,
+    userFavorite: ["ゲーム", "音楽を聴く"]
+};
+
+console.log(`名前：${user.userName}`);
+console.log(`年齢：${user.userAge}歳`);
+console.log(`趣味：${user.userFavorite[0]}, ${user.userFavorite[1]}  など`);
